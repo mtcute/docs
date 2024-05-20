ee6e31cdb920f94d6a1055d6c51540dc8cb7ce5f
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-05-20 20:21:01 +0300

    feat: add view transition on toggle-appearance

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,5 +1,4 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 
@@ -7,19 +6,17 @@ import vitepressBackToTop from 'vitepress-plugin-back-to-top'
 import 'vitepress-plugin-back-to-top/dist/style.css'
 
 // @ts-ignore
+import EmbedPost from '../components/EmbedPost.vue'
+// @ts-ignore
 import VImg from '../components/VImg.vue'
 // @ts-ignore
-import EmbedPost from '../components/EmbedPost.vue'
+import Layout from './Layout.vue'
 
 import './style.css'
 
 export default {
   extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
+  Layout,
   enhanceApp({ app, router, siteData }) {
     app.component('v-img', VImg)
     app.component('EmbedPost', EmbedPost)
